572a593d9347313ab4f591ea30eb9af490dfaa40
seriouslyEnter <not@public.com>
2017-10-11 00:38:58 +0200

    added button and checkboxes to baview

--- src/main/java/dik/adp/app/gui/ba/BaPresenter.java
+++ src/main/java/dik/adp/app/gui/ba/BaPresenter.java
@@ -7,7 +7,15 @@ package dik.adp.app.gui.ba;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 /**
  *
@@ -15,11 +23,42 @@ import javafx.fxml.Initializable;
  */
 public class BaPresenter implements Initializable {
 
-    private ResourceBundle resources = null;
+    @FXML
+    HBox baHBox;
+
+    @FXML
+    ToggleGroup baToggleGroup;
     
+    @FXML
+    GridPane baGridPane;
+
+    private ResourceBundle resources = null;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        //Notwendig weil das für Injected fxml das nicht nicht SceneBuilder gesetzt werden kann
+        AnchorPane.setLeftAnchor(baHBox, 0.0);
+        AnchorPane.setRightAnchor(baHBox, 0.0);
+        AnchorPane.setTopAnchor(baHBox, 0.0);
+        AnchorPane.setBottomAnchor(baHBox, 0.0);
+
         this.resources = resources;
     }
-    
+
+    @FXML
+    void pressToggleButton(ActionEvent event) {
+        ToggleButton selectedTB = (ToggleButton) baToggleGroup.getSelectedToggle();
+        System.out.println("ToggleButton: " + selectedTB.getId());
+
+        for (int i = 1; i < 5; i++) {
+//            hier db abfrage
+            for (int j = 1; j < 7; j++) {
+                CheckBox cb = new CheckBox();
+                baGridPane.add(cb, j, i);
+            }
+        }
+
+    }
+
 }
